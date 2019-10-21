f86b0e4d2522f062140fe50e472e833b58afa05e
sugiout <56539224+sugiout@users.noreply.github.com>
2019-10-21 13:38:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
         //これまでのキャンプのところ
     $('#mada').click(
         function(){
-        $('#mada').text('ごめんね まだないよ').css('color','red');
+        $('#mada').text('右の写真が変わるよ').css('color','red');
     
     });
 
@@ -69,8 +69,57 @@
 
 
 
+  
+   // スライド　しゃしんをいろいろする機能=============================================================================
+   
+   
+
+   $('.index-btn').click(function() {
+    $('.activephoto').removeClass('activephoto');
+    
+    // 変数clickedIndexを定義し、クリックしたボタンのインデックス番号を代入してください
+    var clickedIndex = $('.index-btn').index(this);
+    
+    // eqの引数をclickedIndexに書き換えてください
+    $('.syasin').eq(clickedIndex+1).addClass('activephoto');
+    
+  });
 
 
+   
+  
+
+// スライド　「次へ」と「前へ」の機能　　しゃしんをいろいろする機能========================================================================
+   function toggleChangeBtn() {
+    var slideIndex = $('.syasin').index($('.activephoto'));
+    $('.change-btn').show();
+    if (slideIndex == 0) {
+      $('.prev-btn').hide();
+   
+    } else if (slideIndex == $('.syasin').length-1) {
+      $('.next-btn').hide();
+    }
+  }
+  
+  $('.index-btn').click(function() {
+    $('.activephoto').removeClass('activephoto');
+    var clickedIndex = $('.index-btn').index($(this));
+    $('.syasin').eq(clickedIndex).addClass('activephoto');
+    toggleChangeBtn();
+  });
+  
+  $('.change-btn').click(function() {
+    var $displaySlide = $('.activephoto');
+    $displaySlide.removeClass('activephoto');
+    if ($(this).hasClass('next-btn')) {
+      $displaySlide.next().addClass('activephoto');
+    } 
+    else {
+      $displaySlide.prev().addClass('activephoto');
+    }
+    toggleChangeBtn();
+  });
+   
 
     
   });
